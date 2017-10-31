e914b3ef7d108797cbcf4bad7f9f4d15887bab45
Viktor Drugge <nat11vdr@student.lu.se>
2017-10-31 19:26:26 +0100

    Added skeleton for readfile function

--- Code/project.cpp
+++ Code/project.cpp
@@ -1,13 +1,12 @@
 #include "tempTrender.h"
-#include <string>
 
 
-void project();
+//void project();
 
 void project() {
-	string pathToFile = "../datasets/"; //Put the path to your data file here
+	string pathToFile = "../datasets/smhi-opendata_Lund.csv"; //Put the path to your data file here
 	tempTrender t(pathToFile); //Instantiate your analysis object
-	
+	t.readfile(t.getFilePath(), t.getTempVec());
 	//t.tempOnDay(8, 23); //Call some functions that you've implemented
 	//t.tempOnDay(235);
 	//t.tempPerDay();
@@ -15,3 +14,4 @@ void project() {
 	//t.tempPerYear(2050);
 	//t.aveSpringArrive();
 }
+
--- Code/tempTrender.cpp
+++ Code/tempTrender.cpp
@@ -1,17 +1,34 @@
-//#include <iostream>
 #include "tempTrender.h"
 
-tempTrender::tempTrender(string filePath) {
-	//cout << "The user supplied " << filePath << " as the path to the data file." << endl;
-	setFilePath(filePath);
-}
 
+tempTrender::tempTrender(string filePath) {
+	setFilePath(filePath);
+	//cout << "The user supplied " << getFilePath() << " as the path to the data file." << endl;
+}
 
 void tempTrender::aveSpringArrive(){
 	
 }
 
-vector<double> tempTrender::readfile(string file){
-	
+//Function for reading temperature values to vector temp_vec
+void tempTrender::readfile(string dataFile, vector<double> & temp_vec){
+	temp_vec.clear();
+	cout << "Reading file " << dataFile << " ...\n";
+	stringstream ss;
+	Int_t line = -1; // Counter
+	string date;
+	string time;
+	Double_t temp = 0;
+	string quality;
+
+	ifstream file(dataFile.c_str());
+	Int_t eventNo = -1;
+	//Loop for reading file
+	while(getline(file, date))
+	{
+		line++;
+		file >> date >> time >> temp >> quality;
+	}
+	file.close();
 }
 
