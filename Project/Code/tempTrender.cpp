e9649299fb97431a686b5eab7b05206504429d8d
Viktor Drugge <nat11vdr@student.lu.se>
2017-10-31 14:51:52 +0100

    Removed Project folder

--- Project/Code/tempTrender.cpp
+++
@@ -1,8 +0,0 @@
-#include <iostream>
-#include "tempTrender.h"
-
-tempTrender::tempTrender(string filePath) {
-	cout << "The user supplied " << filePath << " as the path to the data file." << endl;
-	setFilePath(filePath);
-}
-
