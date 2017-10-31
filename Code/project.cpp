dd489ccebed0a26a475b93b4c0adbef45c9f743c
Viktor Drugge <nat11vdr@student.lu.se>
2017-10-31 14:57:05 +0100

    Removed iostream from project file

--- Code/project.cpp
+++ Code/project.cpp
@@ -1,16 +1,12 @@
 #include "tempTrender.h"
 #include <string>
 
-#include <iostream>
-
-using namespace std;
 
 void project();
 
 void project() {
-	string pathToFile = "~/Project/datasets/"; //Put the path to your data file here
+	string pathToFile = "../datasets/"; //Put the path to your data file here
 	tempTrender t(pathToFile); //Instantiate your analysis object
-	cout << t.getFilePath() << endl;
 	
 	//t.tempOnDay(8, 23); //Call some functions that you've implemented
 	//t.tempOnDay(235);
