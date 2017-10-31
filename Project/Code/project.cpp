cfd5a715fbf5c4c3f8477e5972e4bb6e7f2ae860
Viktor Drugge <nat11vdr@student.lu.se>
2017-10-31 13:48:25 +0100

    Commented out cout line and removed using namespace std

--- Project/Code/project.cpp
+++ Project/Code/project.cpp
@@ -1,16 +1,13 @@
 #include "tempTrender.h"
 #include <string>
-
 #include <iostream>
-
-using namespace std;
 
 void project();
 
 void project() {
 	string pathToFile = "~/Project/datasets/"; //Put the path to your data file here
 	tempTrender t(pathToFile); //Instantiate your analysis object
-	cout << t.getFilePath() << endl;
+	//std::cout << t.getFilePath() << std::endl;
 	
 	//t.tempOnDay(8, 23); //Call some functions that you've implemented
 	//t.tempOnDay(235);
