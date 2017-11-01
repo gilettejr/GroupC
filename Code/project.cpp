c31726dbadacf1f8eb4bed4aacb82b4d985f9791
Viktor Drugge <nat11vdr@student.lu.se>
2017-11-01 10:54:06 +0100

    Updated path to uppsala_tm_1722-2013.dat

--- Code/project.cpp
+++ Code/project.cpp
@@ -4,7 +4,7 @@
 //void project();
 
 void project() {
-	string pathToFile = "../datasets/smhi-opendata_Lund.csv"; //Put the path to your data file here
+	string pathToFile = "../datasets/uppsala_tm_1722-2013.dat"; //Put the path to your data file here
 	tempTrender t(pathToFile); //Instantiate your analysis object
 	t.readfile(t.getFilePath(), t.getTempVec());
 	//t.tempOnDay(8, 23); //Call some functions that you've implemented
