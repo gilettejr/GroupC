cfa0c6d61af2a396d127f802ba288a6149d6fbe1
Viktor Drugge <nat11vdr@student.lu.se>
2017-11-01 19:38:54 +0100

    First version of spring function complete

--- Code/tempTrender.cpp
+++ Code/tempTrender.cpp
@@ -13,15 +13,15 @@ void tempTrender::aveSpringArrive(){
 //Function for reading temperature values to vector temp_vec
 //Only work for "uppsala_tm_1722_2013.dat" file
 void tempTrender::readfileSA(string dataFile, vector<double> & tempVec){
-	TH1F *h1 = new TH1F("h1","x distr", 52, 0, 365);
+	TH1F *h1 = new TH1F("h1","x distr", 100, 0, 400);
 	Int_t dayCount = 12;
 	Int_t daysWeek = 7;
-	Int_t year;
-	Int_t month;
-	Int_t day;
+	Int_t year, sYear = 0;
+	Int_t month, sMonth = 0;
+	Int_t day, sDay = 0;
 	Int_t id;
-	Double_t temp = 0;
-	Double_t temp_urban = 0;
+	Double_t temp;
+	Double_t temp_urban;
 	bool foundSpring = false;
 	tempVec.clear();
 	cout << "Reading file " << dataFile << " ...\n";
@@ -30,33 +30,56 @@ void tempTrender::readfileSA(string dataFile, vector<double> & tempVec){
 	//Loop for reading file
 	while(file.good())
 	{
-		//Find first day of spring each year
-		for(Int_t day=0; day < daysWeek; day++)
+		//Iterate till next year if spring found
+		if(foundSpring == true)
 		{
-			Double_t tmp;
-			getline(file,line, '\n');
-			stringstream ss(line);
-			if(ss >> year >> month >> day >> temp >> temp_urban >> id) //check output can is eligible
+			dayCount = 0;
+			foundSpring = false;
+			Int_t nextYear = year+1;
+			while(nextYear != year)
 			{
-				dayCount++; //Increment
-				if(id==1 && temp_urban >= 0 && temp_urban <= 10) //Take data from Uppsala, check if temp is in allowed interval
+				if(getline(file,line, '\n'))
 				{
-					if(day == 0) //Save first entry
-						tmp = dayCount;
-					if(day == daysWeek-1) //Save temp of first day
-					{
-						foundSpring = true;
-						tempVec.push_back(tmp);
-						h1->Fill(tmp);
-					}
-
+					stringstream ssNextYear(line);
+					ssNextYear >> year >> month >> day >> temp >> temp_urban >> id;
 				}
-				else //If temp is not in interval, start new iteration
+				else
 					break;
 			}
 		}
+		//Find first day of spring each year
+		if(foundSpring == false)
+			for(Int_t i=0; i < daysWeek; i++)
+			{
+				Double_t tmp;
+				getline(file,line, '\n');
+				stringstream ss(line);
+				if(ss >> year >> month >> day >> temp >> temp_urban >> id) //check output can is eligible
+				{
+					dayCount++;
+					if(id==1)//Take data from Uppsala
+					{
+						//dayCount>=46 represent 15 feb (minimum date for spring), month<8 remove missing data (otherwise autumn is classified as spring)
+						if(temp_urban >= 0 && temp_urban <= 10 && dayCount >= 46 && month < 8) //Check if temp fulfill definition of spring
+						{
+							if(i==0){
+								sYear=year; sMonth=month; sDay=day;
+							}
+							if(i == daysWeek-1) //Save temp of first day
+							{
+								foundSpring = true;
+								tempVec.push_back(dayCount - (daysWeek+1)); //(daysWeek+1), dayCount incremented by 1 previously
+								h1->Fill(dayCount - (daysWeek+1));
+								//Print date of spring
+								cout << "Spring found:\t" << sYear << "\t" << sMonth << "\t" << sDay << "\t" << endl;
+							}
+						}
+						else //If temperature is not in interval, start new iteration
+							break;
+					}
+				}
+			}
 	}
-	cout << foundSpring << endl;
 	file.close();
 	h1->Draw();
 }
